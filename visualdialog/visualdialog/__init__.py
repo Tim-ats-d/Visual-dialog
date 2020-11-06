480e43f70671094527c4a578b5be4dd32edca5da
Tim-ats-d <darkogm69@gmail.com>
2020-11-06 22:42:44 +0100

    Add argument flash_screen to char_by_char and word_by_word methods, add downtime_chars attributs to DialogBox class, formmating arborescence to ready to publish on pypy.

---
+++ visualdialog/visualdialog/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+A librairie which provides class and functions to make easier dialog box in terminal.
+"""
+
+__version__ = 0.2
+
+from core import DialogBox
+from core import init_visualdialog
